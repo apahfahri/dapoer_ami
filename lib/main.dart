50683d4e2e5f38824673d7d72fe66867b3b027a5
apahfahri <ahmadfahrie19@gmail.com>
2025-06-30 14:34:39 +0700

    chore: Comment out device preview imports and related code for cleaner main entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
+// import 'package:device_preview/device_preview.dart';
+// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -27,11 +27,11 @@ void main() async {
   );
 
   // Menjalankan aplikasi.
-  // runApp(const MyApp());
-  runApp(DevicePreview(
-    enabled: !kReleaseMode,
-    builder: (context) => const MyApp(),
-  ));
+  runApp(const MyApp());
+  // runApp(DevicePreview(
+  //   enabled: !kReleaseMode,
+  //   builder: (context) => const MyApp(),
+  // ));
 }
 
 // MyApp adalah widget root dari aplikasi Anda.
